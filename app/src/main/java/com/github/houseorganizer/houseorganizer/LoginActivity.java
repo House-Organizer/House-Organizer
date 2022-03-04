b952869545eb9e8bc235a1c1d25c5488707053bd
Geno22 <florian.marcoz@outlook.fr>
2022-03-04 11:46:15 +0100

    Connection of the project to Firebase
    The project is now registered in Firebase and the Firebase authentication SDK is added to the app.
    The next step is the n°3 in the assistant : completing the set up.

---
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.github.houseorganizer.houseorganizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
