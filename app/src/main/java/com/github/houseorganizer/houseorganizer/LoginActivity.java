c086e667415f8f5e981ef496f66ca41836eefbda
Geno22 <florian.marcoz@outlook.fr>
2022-03-06 18:50:13 +0100

    Added Firebase and Google sign-in dependencies, and added a basic login activity
    Added Firebase and Google sign-in dependencies, and added a simple activity with a non-functionnal button

--- app/src/main/java/com/github/houseorganizer/houseorganizer/LoginActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/LoginActivity.java
@@ -3,14 +3,42 @@ package com.github.houseorganizer.houseorganizer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null){
+            // Launch activity after login
+        }
+        // Else : display login screen
         setContentView(R.layout.activity_login);
+        //Activating the sign-in button
+        //findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+
     }
+
 
 
 }
