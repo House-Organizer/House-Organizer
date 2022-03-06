fa657b70f865ab9b85fcaa512558158eae4bb917
Luca Delabarre <luca.delabarre@epfl.ch>
2022-03-06 21:44:01 +0100

    Added example tests

--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/MainActivityTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/MainActivityTest.java
@@ -8,6 +8,8 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import static org.junit.Assert.assertEquals;
+
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -18,5 +20,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
-
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/MainScreenActivityTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/MainScreenActivityTest.java
@@ -5,6 +5,8 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Intent;
 
 import androidx.test.core.app.ActivityScenario;
@@ -16,5 +18,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class MainScreenActivityTest {
-
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 }
