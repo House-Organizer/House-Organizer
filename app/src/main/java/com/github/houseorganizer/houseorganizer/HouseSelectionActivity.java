a8677d432e10b91a85d4ff98aebb6704a2ad78ab
Sandra <sansive00@gmail.com>
2022-03-10 01:55:44 +0100

    Convert house names into buttons

--- app/src/main/java/com/github/houseorganizer/houseorganizer/HouseSelectionActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/HouseSelectionActivity.java
@@ -15,7 +15,7 @@ public class HouseSelectionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_selection);
+        setContentView(R.layout.activity_house_selection);
 
         housesView = findViewById(R.id.housesView);
 
