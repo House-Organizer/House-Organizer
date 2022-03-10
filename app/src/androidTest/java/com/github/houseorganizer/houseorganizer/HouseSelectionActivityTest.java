1b60e1e86615bc3ee7dcedcad11f2b4059cdf6c8
Sandra <sansive00@gmail.com>
2022-03-10 04:20:19 +0100

    Add buttons tests for code coverage

--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/HouseSelectionActivityTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/HouseSelectionActivityTest.java
@@ -1,8 +1,12 @@
 package com.github.houseorganizer.houseorganizer;
 
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
@@ -10,6 +14,7 @@ import android.content.Intent;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
@@ -30,5 +35,20 @@ public class HouseSelectionActivityTest {
                     .check(matches(hasDescendant(withText("House 3"))))
                     .check(matches(hasDescendant(withText("House 4"))));
         }
+    }
+
+    /* House selected */
+    @Test
+    public void selectHouse() {
+        Intents.init();
+
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), HouseSelectionActivity.class);
+
+        try (ActivityScenario<HouseSelectionActivity> scenario = ActivityScenario.launch(intent)) {
+            onView(withText("House 1")).perform(click());
+            intended(toPackage("com.github.houseorganizer.houseorganizer"));
+        }
+
+        Intents.release();
     }
 }
