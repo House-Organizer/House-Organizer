9d10aca0700047de1bf9c75db599f2c6cd5ed3e7
Sandra <sansive00@gmail.com>
2022-03-10 03:51:42 +0100

    Delete buttons test

--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/HouseSelectionActivityTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/HouseSelectionActivityTest.java
@@ -1,21 +1,15 @@
 package com.github.houseorganizer.houseorganizer;
 
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import android.content.Intent;
-import android.support.test.espresso.contrib.RecyclerViewActions;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
@@ -36,21 +30,5 @@ public class HouseSelectionActivityTest {
                     .check(matches(hasDescendant(withText("House 3"))))
                     .check(matches(hasDescendant(withText("House 4"))));
         }
-    }
-
-    /* House selected */
-    @Test
-    public void selectHouse() {
-        Intents.init();
-
-        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), HouseSelectionActivity.class);
-
-        try (ActivityScenario<HouseSelectionActivity> scenario = ActivityScenario.launch(intent)) {
-            onView(withId(R.id.housesView))
-                    .perform((ViewAction) RecyclerViewActions.actionOnItemAtPosition(3, click()));
-            intended(toPackage("com.github.houseorganizer.houseorganizer"));
-        }
-
-        Intents.release();
     }
 }
