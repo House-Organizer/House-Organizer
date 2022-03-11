5eaadc6b1f486d98e89c79fefbcdefdc403d4ba2
germano <maxime.germano@epfl.ch>
2022-03-11 17:36:33 +0100

    Add LoginActivityTest

--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/LoginActivityTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/LoginActivityTest.java
@@ -1,11 +1,15 @@
 package com.github.houseorganizer.houseorganizer;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -28,6 +32,15 @@ public class LoginActivityTest {
     @Test
     public void signInButtonIsEnabled() {
         onView(withId(R.id.google_sign_in_button)).check(matches(isEnabled()));
+    }
+
+    // Test that when the sign in button is clicked an intent is fired
+    @Test
+    public void clickSignInButtonFiresIntent() {
+        Intents.init();
+        onView(withId(R.id.google_sign_in_button)).perform(click());
+        intended(toPackage("com.github.houseorganizer.houseorganizer"));
+        Intents.release();
     }
 
     /* Discover button */
