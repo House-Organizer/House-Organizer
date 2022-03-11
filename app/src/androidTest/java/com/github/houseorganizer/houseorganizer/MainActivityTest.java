e8865adf1aa8963db9ef1203b17017e4e2f40055
germano <maxime.germano@epfl.ch>
2022-03-11 17:44:02 +0100

    Add MainActivityTest

---
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/MainActivityTest.java
@@ -0,0 +1,41 @@
+package com.github.houseorganizer.houseorganizer;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> testRule = new ActivityScenarioRule<>(LoginActivity.class);
+
+    /* See logo */
+    @Test
+    public void seeHousesLogo() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), MainActivity.class);
+
+        try (ActivityScenario<HouseSelectionActivity> scenario = ActivityScenario.launch(intent)) {
+            onView(withId(R.id.house_logo)).check(matches(isDisplayed()));
+        }
+    }
+}
