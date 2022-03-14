60b9c2382e554591da901f70b5e8adf36acf224b
germano <maxime.germano@epfl.ch>
2022-03-14 18:53:47 +0100

    Fix bug where app would crash on "Sign in with email"

--- app/src/main/java/com/github/houseorganizer/houseorganizer/LoginEmail.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/LoginEmail.java
@@ -11,15 +11,11 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginEmail extends AppCompatActivity {
+public class LoginEmail extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
 
     private final String TAG = "LoginEmail";
-
-    private final EditText email_field = findViewById(R.id.enter_email);
-    private final EditText password_field = findViewById(R.id.enter_password);
-    private final TextView error_message = findViewById(R.id.login_email_error_message);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +24,28 @@ public class LoginEmail extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        findViewById(R.id.emailactivity_signin_button).setOnClickListener(this);
+        findViewById(R.id.email_activity_singup_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.emailactivity_signin_button:
+                signInWithEmail(v);
+                break;
+            case R.id.email_activity_singup_button:
+                signUpWithEmail(v);
+                break;
+        }
     }
 
     public void signInWithEmail(View v) {
+        EditText email_field = findViewById(R.id.enter_email);
+        EditText password_field = findViewById(R.id.enter_password);
         String email = email_field.getText().toString();
         String password = password_field.getText().toString();
+        TextView error_message = findViewById(R.id.login_email_error_message);
 
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
@@ -50,8 +63,11 @@ public class LoginEmail extends AppCompatActivity {
     }
 
     public void signUpWithEmail(View v) {
+        EditText email_field = findViewById(R.id.enter_email);
+        EditText password_field = findViewById(R.id.enter_password);
         String email = email_field.getText().toString();
         String password = password_field.getText().toString();
+        TextView error_message = findViewById(R.id.login_email_error_message);
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
