34a77be3e9ac04c7d1a50e52f218303b845d39d8
germano <maxime.germano@epfl.ch>
2022-03-15 15:43:26 +0100

    Modifiy LoginEmail activity to enable the register activity.

--- app/src/main/java/com/github/houseorganizer/houseorganizer/LoginEmail.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/LoginEmail.java
@@ -2,6 +2,7 @@ package com.github.houseorganizer.houseorganizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,28 +25,29 @@ public class LoginEmail extends AppCompatActivity implements View.OnClickListene
 
         mAuth = FirebaseAuth.getInstance();
 
-        findViewById(R.id.emailactivity_signin_button).setOnClickListener(this);
-        findViewById(R.id.email_activity_singup_button).setOnClickListener(this);
+        findViewById(R.id.log_email_signin_button).setOnClickListener(this);
+        findViewById(R.id.log_email_register_button).setOnClickListener(this);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.emailactivity_signin_button:
+            case R.id.log_email_signin_button:
                 signInWithEmail(v);
                 break;
-            case R.id.email_activity_singup_button:
-                signUpWithEmail(v);
+            case R.id.log_email_register_button:
+                startActivity(new Intent(LoginEmail.this, RegisterEmail.class));
                 break;
         }
     }
 
     public void signInWithEmail(View v) {
-        EditText email_field = findViewById(R.id.enter_email);
-        EditText password_field = findViewById(R.id.enter_password);
+        EditText email_field = findViewById(R.id.log_enter_email);
+        EditText password_field = findViewById(R.id.log_enter_password);
         String email = email_field.getText().toString();
         String password = password_field.getText().toString();
-        TextView error_message = findViewById(R.id.login_email_error_message);
+        TextView error_message = findViewById(R.id.log_email_error_message);
 
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
@@ -57,29 +59,7 @@ public class LoginEmail extends AppCompatActivity implements View.OnClickListene
                     } else {
                         // If sign in fails, display a message to the user.
                         Log.d(TAG, "signInWithGoogleCredential:failure");
-                        error_message.setText("Email or password is wrong.");
-                    }
-                });
-    }
-
-    public void signUpWithEmail(View v) {
-        EditText email_field = findViewById(R.id.enter_email);
-        EditText password_field = findViewById(R.id.enter_password);
-        String email = email_field.getText().toString();
-        String password = password_field.getText().toString();
-        TextView error_message = findViewById(R.id.login_email_error_message);
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success");
-                        startActivity(new Intent(LoginEmail.this, MainScreenActivity.class));
-                        finish();
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                        error_message.setText("Authentication failed.");
+                        error_message.setText(R.string.log_email_auth_failed);
                     }
                 });
     }
