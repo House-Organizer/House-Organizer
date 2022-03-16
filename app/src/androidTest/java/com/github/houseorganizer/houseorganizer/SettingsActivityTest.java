6a3ee591f3732a260d54ff8c4b1c050af42eb601
Luca Delabarre <luca.delabarre@epfl.ch>
2022-03-16 18:12:05 +0100

    Added test class for settings. Did not manage to test the settings fragment yet

---
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/SettingsActivityTest.java
@@ -0,0 +1,32 @@
+package com.github.houseorganizer.houseorganizer;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SettingsActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<SettingsActivity> testRule = new ActivityScenarioRule<SettingsActivity>(SettingsActivity.class);
+
+    @Test
+    public void canInputTextInSettings() {
+        //TODO
+    }
+
+}
