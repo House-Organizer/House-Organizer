418ed4035f76b59423a263f8d385b6f54b397060
aindreias <anamaria.indreias@gmail.com>
2022-03-16 13:28:19 +0100

    Complete left-over unit tests for TaskList

--- app/src/test/java/com/github/houseorganizer/houseorganizer/TaskListUnitTest.java
+++ app/src/test/java/com/github/houseorganizer/houseorganizer/TaskListUnitTest.java
@@ -93,12 +93,51 @@ public class TaskListUnitTest {
     }
 
     @Test
-    public void taskListCanRemoveFinishedTasksWithoutRemovedSubTasks() {
-        // todo
+    public void taskListCanRemoveFinishedTasks() {
+        Task t  = new Task(TaskUnitTest.NOBODY, "Task 1", "description 1");
+        Task t2 = new Task(TaskUnitTest.NOBODY, "Task 2", "description 2");
+        Task t3 = new Task(TaskUnitTest.NOBODY, "Task 3", "description 3");
+
+        TaskList tl = new TaskList(TaskUnitTest.NOBODY, "TaskList 1", new ArrayList<>());
+
+        tl.addTask(t);
+        tl.addTask(t2);
+        tl.addTask(t3);
+
+        t.markAsFinished();
+        t2.markAsFinished();
+
+        tl.removeFinishedTasks(false);
+        assertEquals(t3, tl.getTaskAt(0));
+
+        t3.markAsFinished();
+
+        tl.removeFinishedTasks(false);
+        assertFalse(tl.hasTasks());
     }
 
     @Test
     public void taskListCanRemoveFinishedTasksAndSubTasks() {
-        // todo
+        Task t  = new Task(TaskUnitTest.NOBODY, "Task 1", "description 1");
+        Task t2 = new Task(TaskUnitTest.NOBODY, "Task 2", "description 2");
+
+        TaskList tl = new TaskList(TaskUnitTest.NOBODY, "TaskList 1", new ArrayList<>());
+        tl.addTask(t);
+        tl.addTask(t2);
+
+        // Add 2 subtasks to task#2, mark first as finished
+        // + mark task#1 as finished
+        Task.SubTask s = new Task.SubTask("SubTask 1");
+        Task.SubTask s2 = new Task.SubTask("SubTask 2");
+
+        t2.addSubTask(s);
+        t2.addSubTask(s2);
+
+        t2.markSubTaskAsFinished(0); // s marked as finished
+        t.markAsFinished();
+
+        tl.removeFinishedTasks(true);
+        assertEquals(t2, tl.getTaskAt(0));
+        assertEquals(s2, tl.getTaskAt(0).getSubTaskAt(0));
     }
 }
