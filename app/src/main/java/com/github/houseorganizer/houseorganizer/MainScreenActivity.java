0267594085da22e85ee3d8ad00df3ed63c59c159
Thibaud <thibaud.banderet@gmail.com>
2022-03-16 17:01:35 +0100

    refactor onCreate to modules

--- app/src/main/java/com/github/houseorganizer/houseorganizer/MainScreenActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/MainScreenActivity.java
@@ -33,15 +33,13 @@ public class MainScreenActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DocumentReference currentHouse;
     private EventsAdapter calendarAdapter;
+    private RecyclerView calendarEvents = findViewById(R.id.calendar);
 
     @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-
-        RecyclerView calendarEvents = findViewById(R.id.calendar);
-
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -53,40 +51,47 @@ public class MainScreenActivity extends AppCompatActivity {
         calendarEvents.setAdapter(calendarAdapter);
         calendarEvents.setLayoutManager(new GridLayoutManager(this, calendarColumns));
 
-        findViewById(R.id.sign_out_button).setOnClickListener(v -> {
-            mAuth.signOut();
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
-        });
+        findViewById(R.id.sign_out_button).setOnClickListener(this::signOut);
+        findViewById(R.id.calendar_view_change).setOnClickListener(this::rotateView);
+        findViewById(R.id.add_event).setOnClickListener(this::addEvent);
+        findViewById(R.id.refresh_calendar).setOnClickListener(this::refreshCalendar);
+    }
 
-        findViewById(R.id.calendar_view_change).setOnClickListener(v -> {
-            calendar.rotateView();
-            calendarColumns = calendar.getView() == Calendar.CalendarView.UPCOMING ? 1 : 7;
-            calendarEvents.setAdapter(calendarAdapter);
-            calendarEvents.setLayoutManager(new GridLayoutManager(this, calendarColumns));
-        });
+    private void signOut(View v) {
+        mAuth.signOut();
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
 
-        findViewById(R.id.add_event).setOnClickListener(v -> {
-            Map<String, Object> data = new HashMap<>();
-            Event event = new Event("added", "this is the event that i added using the add button", LocalDateTime.now(), 100);
-            data.put("title", "added");
-            data.put("description", "this is the event that i added using the add button");
-            data.put("start", LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
-            data.put("duration", 100);
-            data.put("household", currentHouse);
-            LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
-            db.collection("events").add(data)
-                    .addOnSuccessListener(documentReference -> {
-                        Toast.makeText(v.getContext(), v.getContext().getString(R.string.add_success), Toast.LENGTH_SHORT).show();
-                        ArrayList<Event> newEvents = new ArrayList<>(calendar.getEvents());
-                        newEvents.add(event);
-                        calendarAdapter.notifyDataSetChanged();
-                        calendar.setEvents(newEvents);
-                    })
-                    .addOnFailureListener(documentReference -> Toast.makeText(v.getContext(), v.getContext().getString(R.string.add_fail), Toast.LENGTH_SHORT).show());
-        });
+    private void rotateView(View v) {
+        calendar.rotateView();
+        calendarColumns = calendar.getView() == Calendar.CalendarView.UPCOMING ? 1 : 7;
+        calendarEvents.setAdapter(calendarAdapter);
+        calendarEvents.setLayoutManager(new GridLayoutManager(this, calendarColumns));
+    }
 
-        findViewById(R.id.refresh_calendar).setOnClickListener(v -> db.collection("events")
+    private void addEvent(View v) {
+        Map<String, Object> data = new HashMap<>();
+        Event event = new Event("added", "this is the event that i added using the add button", LocalDateTime.now(), 100);
+        data.put("title", "added");
+        data.put("description", "this is the event that i added using the add button");
+        data.put("start", LocalDateTime.now().toEpochSecond(ZoneOffset.UTC));
+        data.put("duration", 100);
+        data.put("household", currentHouse);
+        LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        db.collection("events").add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(v.getContext(), v.getContext().getString(R.string.add_success), Toast.LENGTH_SHORT).show();
+                    ArrayList<Event> newEvents = new ArrayList<>(calendar.getEvents());
+                    newEvents.add(event);
+                    calendarAdapter.notifyDataSetChanged();
+                    calendar.setEvents(newEvents);
+                })
+                .addOnFailureListener(documentReference -> Toast.makeText(v.getContext(), v.getContext().getString(R.string.add_fail), Toast.LENGTH_SHORT).show());
+    }
+
+    private void refreshCalendar(View v) {
+        db.collection("events")
                 .whereEqualTo("household", currentHouse)
                 .get()
                 .addOnCompleteListener(task -> {
@@ -108,8 +113,9 @@ public class MainScreenActivity extends AppCompatActivity {
                     else {
                         Toast.makeText(v.getContext(), v.getContext().getString(R.string.refresh_fail), Toast.LENGTH_SHORT).show();
                     }
-                }));
+                });
     }
+
 
     @SuppressWarnings("unused")
     public void houseButtonPressed(View view) {
