528237c6e29301a8fa77c83edbd1b357c62051d5
Geno22 <florian.marcoz@outlook.fr>
2022-03-17 12:57:00 +0100

    Removed unused import

--- app/src/main/java/com/github/houseorganizer/houseorganizer/MainScreenActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/MainScreenActivity.java
@@ -2,7 +2,6 @@ package com.github.houseorganizer.houseorganizer;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
