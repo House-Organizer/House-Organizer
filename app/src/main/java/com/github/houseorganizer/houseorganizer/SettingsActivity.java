64562f369742af810c1f4777659aa00737f50689
Luca Delabarre <luca.delabarre@epfl.ch>
2022-03-19 18:04:01 +0100

    Linked settings name to firebase

--- app/src/main/java/com/github/houseorganizer/houseorganizer/SettingsActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/SettingsActivity.java
@@ -1,17 +1,29 @@
 package com.github.houseorganizer.houseorganizer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class SettingsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
@@ -24,10 +36,42 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        private FirebaseFirestore db;
+        private FirebaseUser Uid;
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            db = FirebaseFirestore.getInstance();
+            Uid = FirebaseAuth.getInstance().getCurrentUser();
+            getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+            if(s.equals("nickname")){
+                Map<String, Object> user_nickname = new HashMap<>();
+                user_nickname.put(Uid.getUid(), sharedPreferences.getString("nickname",""));
+                db.collection("uid-to-nickname")
+                   .document("uid-to-nickname-translations")
+                   .update(user_nickname);
+            }
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
         }
     }
+
+
 }
