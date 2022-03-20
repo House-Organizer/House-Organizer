972a17617f38214208d46c1abb7dda49457d7156
Geno22 <florian.marcoz@outlook.fr>
2022-03-20 16:05:55 +0100

    Implemented ShopList

--- app/src/main/java/com/github/houseorganizer/houseorganizer/ShopList.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/ShopList.java
@@ -7,12 +7,54 @@ public class ShopList {
 
     private final User owner;
     private List<ShopItem> items;
-    private String title;
+    private String name;
 
-    public ShopList(User owner, String title){
+    public ShopList(User owner, String listName){
         this.owner = owner;
-        this.title = title;
+        this.name = listName;
         items = new ArrayList<>();
     }
 
+    public ShopList(User owner, String listName, List<ShopItem> list){
+        this.owner = owner;
+        this.name = listName;
+        items = new ArrayList<>(list);
+    }
+
+    public void addItem(ShopItem item){
+        items.add(item);
+    }
+
+    public void removeItem(ShopItem item){
+        items.remove(item);
+    }
+
+    public void removeItem(int index){
+        items.remove(index);
+    }
+
+    public void removedPickedUpItems(){
+        items.removeIf(ShopItem::isPickedUp);
+    }
+
+    public boolean isEmpty(){
+        return items.isEmpty();
+    }
+
+    //Getters
+    public String getListName() {
+        return name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public int size(){
+        return items.size();
+    }
+
+    public ShopItem getItemAt(int index){
+        return items.get(index);
+    }
 }
