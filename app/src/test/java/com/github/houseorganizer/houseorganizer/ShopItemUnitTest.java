d1e357a99f83505554991bc2d30ba8ed83c31a79
Geno22 <34856793+Geno22@users.noreply.github.com>
2022-03-21 09:28:11 +0100

    Item toString, fixed display of items

--- app/src/test/java/com/github/houseorganizer/houseorganizer/ShopItemUnitTest.java
+++ app/src/test/java/com/github/houseorganizer/houseorganizer/ShopItemUnitTest.java
@@ -1,4 +1,52 @@
 package com.github.houseorganizer.houseorganizer;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
 public class ShopItemUnitTest {
+
+    @Test
+    public void constructorBuildsItem(){
+        ShopItem item = new ShopItem("Egg", 3, "g");
+        assertThat(item, is(notNullValue()));
+    }
+
+    @Test
+    public void markPickedUpWorks(){
+        ShopItem item = new ShopItem("name", 4, "");
+        item.markPickedUp();
+        assertThat(item.isPickedUp(), is(true));
+    }
+
+    // Testing setters
+
+    @Test
+    public void changeNameWorks(){
+        ShopItem item = new ShopItem("name", 4, "");
+        String newName = "myNewName";
+        item.changeName(newName);
+        assertThat(item.getName(), is(newName));
+    }
+
+    @Test
+    public void setQuantityWorks(){
+        ShopItem item = new ShopItem("name", 4, "");
+        int quant = 5;
+        item.setQuantity(quant);
+        assertThat(item.getQuantity(), is(quant));
+    }
+
+    @Test
+    public void setUnitWorks(){
+        ShopItem item = new ShopItem("name", 4, "");
+        String unit = "omegatonnes";
+        item.setUnit(unit);
+        assertThat(item.getUnit(), is(unit));
+    }
+
+
 }
