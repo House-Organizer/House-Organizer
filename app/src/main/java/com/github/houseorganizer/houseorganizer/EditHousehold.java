6ae81af05a6e32ba9ad28852d6054241cbcaf6ee
Luca Delabarre <luca.delabarre@epfl.ch>
2022-03-29 17:02:31 +0200

    Removed temporarily recyclerview of the residents Adapted current code to display name of house and send correct intent to the main activity

--- app/src/main/java/com/github/houseorganizer/houseorganizer/EditHousehold.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/EditHousehold.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,23 +19,25 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class EditHousehold extends AppCompatActivity {
     RecyclerView usersView;
     FirebaseFirestore firestore;
     FirebaseAuth mAuth;
+    private String householdId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_household);
 
-        usersView = findViewById(R.id.usersView);
+        //usersView = findViewById(R.id.usersView);
         mAuth = FirebaseAuth.getInstance();
         Context cx = getApplicationContext();
 
         Intent intent = getIntent();
-        String householdId = intent.getStringExtra(MainScreenActivity.HOUSEHOLD);
+        this.householdId = intent.getStringExtra(MainScreenActivity.HOUSEHOLD);
 
         firestore = FirebaseFirestore.getInstance();
         firestore.collection("households")
@@ -44,18 +47,35 @@ public class EditHousehold extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         DocumentSnapshot document = task.getResult();
-                        List<String> usersUid = (List<String>) document.get("residents");
+                        Map<String, Object> householdData = document.getData();
+                        if(householdData != null) {
+                            TextView tv = findViewById(R.id.edit_household_name);
+                            tv.setText(householdData
+                              .getOrDefault("name", "Could not retrieve name")
+                              .toString()); //Ignore IDE null warning as check is done above
+                        }
+                    }
+                });
 
+        firestore.collection("households")
+                .document(householdId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        List<String> usersUid = (List<String>) document.get("residents");
+                        /*
                         UserAdapter adapter = new UserAdapter(cx, usersUid);
                         usersView.setAdapter(adapter);
-                        usersView.setLayoutManager(new LinearLayoutManager(cx));
+                        usersView.setLayoutManager(new LinearLayoutManager(cx));*/
                     }
                 });
     }
 
     public void confirmChanges(View view) {
         Intent intent = new Intent(this, MainScreenActivity.class);
-        intent.putExtra(MainScreenActivity.HOUSEHOLD, "test_house_0");
+        intent.putExtra(MainScreenActivity.HOUSEHOLD, householdId);
         startActivity(intent);
     }
 }
