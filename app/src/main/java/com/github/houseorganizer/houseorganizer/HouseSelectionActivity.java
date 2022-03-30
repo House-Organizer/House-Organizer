7781f5933b9947a264200493a111075bf65aec75
Luca Delabarre <luca.delabarre@epfl.ch>
2022-03-30 13:31:34 +0200

    Added check of house ownership in order to access edit screen

--- app/src/main/java/com/github/houseorganizer/houseorganizer/HouseSelectionActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/HouseSelectionActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +16,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+
+import java.util.List;
+import java.util.Map;
 
 public class HouseSelectionActivity extends AppCompatActivity {
     RecyclerView housesView;
     String emailUser;
     FirestoreRecyclerAdapter<HouseModel, HouseViewHolder> adapter;
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +40,9 @@ public class HouseSelectionActivity extends AppCompatActivity {
 
         housesView = findViewById(R.id.housesView);
         emailUser = getIntent().getStringExtra(MainScreenActivity.CURRENT_USER);
+        this.firestore = FirebaseFirestore.getInstance();
 
-        Query query = FirebaseFirestore.getInstance().collection("households").whereArrayContains("residents", emailUser);
+        Query query = firestore.collection("households").whereArrayContains("residents", emailUser);
         FirestoreRecyclerOptions<HouseModel> options = new FirestoreRecyclerOptions.Builder<HouseModel>()
                 .setQuery(query, HouseModel.class).build();
         adapter = new FirestoreRecyclerAdapter<HouseModel, HouseViewHolder>(options) {
@@ -64,6 +74,31 @@ public class HouseSelectionActivity extends AppCompatActivity {
     }
 
     public void editHousehold(View view) {
+        String householdId = view.getTag().toString();
+
+        firestore.collection("households")
+                .document(householdId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        Map<String, Object> householdData = document.getData();
+                        if(householdData != null) {
+                            String owner = (String) householdData.getOrDefault("owner", null);
+                            if(owner == null || !owner.equals(emailUser)) {
+                                Toast.makeText(getApplicationContext(),
+                                        view.getContext().getString(R.string.not_owner),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                sendToEditHouse(view);
+                            }
+                        }
+                    }
+                });
+    }
+
+    public void sendToEditHouse(View view){
         Intent intent = new Intent(this, EditHousehold.class);
         intent.putExtra(MainScreenActivity.HOUSEHOLD, view.getTag().toString());
         startActivity(intent);
