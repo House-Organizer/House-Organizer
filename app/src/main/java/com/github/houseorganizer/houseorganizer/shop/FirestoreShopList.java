f8ff8278fa2bca58f1aca2a302eacbbd0cc11789
Geno22 <florian.marcoz@outlook.fr>
2022-04-06 11:41:11 +0200

    Continuing FirestoreShopList

--- app/src/main/java/com/github/houseorganizer/houseorganizer/shop/FirestoreShopList.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/shop/FirestoreShopList.java
@@ -2,12 +2,14 @@ package com.github.houseorganizer.houseorganizer.shop;
 
 import com.github.houseorganizer.houseorganizer.user.User;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class FirestoreShopList extends ShopList{
     private String houseHoldID;
@@ -23,6 +25,25 @@ public class FirestoreShopList extends ShopList{
         this.shopListDocRef = docRef;
     }
 
+    public static void storeShopList(ShopList shopList, CollectionReference shopListListRoot, String documentName) throws ExecutionException, InterruptedException {
+        Map<String, Object> newList = new HashMap<>();
+
+        newList.put("name", shopList.getListName());
+        newList.put("owner", shopList.getOwner());
+
+        Task<Void> t = shopListListRoot.document(documentName).set(newList);
+        Tasks.await(t);
+
+        if(t.isSuccessful()){
+            DocumentReference docRef = shopListListRoot.document(documentName);
+            CollectionReference shopListRef = docRef.collection("groceries");
+
+            for(int i = 0; i < shopList.size(); ++i){
+                Tasks.await(storeShopItem(shopList.getItemAt(i), shopListRef));
+            }
+        }
+    }
+
     public static Task<DocumentReference> storeShopItem(ShopItem shopItem, CollectionReference firestoreShopList){
         Map<String, Object> toStore = new HashMap<>();
 
@@ -30,6 +51,7 @@ public class FirestoreShopList extends ShopList{
         toStore.put("name", shopItem.getName());
         toStore.put("quantity", shopItem.getQuantity());
         toStore.put("unit", shopItem.getUnit());
+        toStore.put("pickedUp", shopItem.isPickedUp());
 
         return firestoreShopList.add(toStore);
     }
