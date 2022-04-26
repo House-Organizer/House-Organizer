bed41c27aab91c0411493e06a5feab6afcd1a2ca
germano <maxime.germano@epfl.ch>
2022-04-26 18:48:29 +0200

    Create methods initResidents, initBalances and computeBalances

--- app/src/main/java/com/github/houseorganizer/houseorganizer/billsharer/Billsharer.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/billsharer/Billsharer.java
@@ -1,13 +1,28 @@
 package com.github.houseorganizer.houseorganizer.billsharer;
 
+import android.util.Log;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Billsharer {
 
     private ArrayList<Expense> expenses;
+    private ArrayList<Debt> debts;
+    private HashMap<String, Integer> balances;
+    private DocumentReference currentHouse;
+    private ArrayList<String> residents;
 
-    public Billsharer() {
+    public Billsharer(DocumentReference currentHouse) {
         expenses = new ArrayList<>();
+        debts = new ArrayList<>();
+        this.currentHouse = currentHouse;
+        initResidents();
+        initBalances();
+        computeBalances();
     }
 
     public ArrayList<Expense> getExpenses() {
@@ -18,5 +33,73 @@ public class Billsharer {
         this.expenses = expenses;
     }
 
+    public ArrayList<Debt> getDebts() {
+        return debts;
+    }
 
+    public void setDebts(ArrayList<Debt> debts) {
+        this.debts = debts;
+    }
+
+    public HashMap<String, Integer> getBalances() {
+        return balances;
+    }
+
+    public void setBalances(HashMap<String, Integer> balances) {
+        this.balances = balances;
+    }
+
+    public ArrayList<String> getResidents() {
+        return residents;
+    }
+
+    public void setResidents(ArrayList<String> residents) {
+        this.residents = residents;
+    }
+
+    public void initResidents() {
+        currentHouse.get().addOnCompleteListener(t -> {
+            if (t.isSuccessful()) {
+                DocumentSnapshot house = t.getResult();
+                setResidents((ArrayList<String>) house.get("residents"));
+            } else {
+                Log.e("Billsharer", "initResidents:could not fetch users");
+            }
+        });
+    }
+
+    public void initBalances() {
+        balances = new HashMap<>();
+        for (String resident : residents) {
+            balances.put(resident, 0);
+        }
+    }
+
+    public void computeDepts() {}
+
+    private int computeTotal(String resident, Expense expense) {
+        int total = 0;
+        if (balances.containsKey(resident)) {
+            total = balances.get(resident);
+        }
+        if (expense.getPayee().equals(resident)) {
+            total += expense.getCost() - expense.getShares().get(resident);
+        } else {
+            total -= expense.getShares().get(resident);
+        }
+
+        return total;
+    }
+
+    public void computeBalances() {
+        for (Expense expense : expenses) {
+            for (String resident : residents) {
+                if (expense.getShares().containsKey(resident)) {
+                    balances.put(resident, computeTotal(resident, expense));
+                } else {
+                    Log.e("Billsharer", "computeBalance:resident not found in shared list");
+                }
+            }
+        }
+    }
 }
