5d0a074451461ec24405e125a00ef3476780c120
Luca Delabarre <luca.delabarre@epfl.ch>
2022-04-26 22:55:25 +0200

    Added possibility to edit the text field

--- app/src/main/java/com/github/houseorganizer/houseorganizer/panels/InfoActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/panels/InfoActivity.java
@@ -2,6 +2,8 @@ package com.github.houseorganizer.houseorganizer.panels;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +17,9 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -54,6 +58,19 @@ public class InfoActivity extends AppCompatActivity {
         } else {
             TextView text = findViewById(R.id.infoHeader);
             text.setText(R.string.no_household_info);
+        }
+    }
+
+    public void changeNotesHousehold(View view){
+        EditText editNotes = findViewById(R.id.editTextHouseholdNotes);
+        String notes = editNotes.getText().toString();
+
+        Map<String, Object> newNotes = new HashMap<>();
+        newNotes.put("notes", notes);
+        if (currentHouse != null) {
+            currentHouse.update(newNotes);
+            TextView notesText = findViewById(R.id.notesTextView);
+            notesText.setText(notes);
         }
     }
 
