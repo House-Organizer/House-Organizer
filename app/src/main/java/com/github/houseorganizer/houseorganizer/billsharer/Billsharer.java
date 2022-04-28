e22ad2a6641b3177b1baf5f80a0c29d454529699
germano <maxime.germano@epfl.ch>
2022-04-28 18:52:28 +0200

    Add methods to Billsharer

--- app/src/main/java/com/github/houseorganizer/houseorganizer/billsharer/Billsharer.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/billsharer/Billsharer.java
@@ -2,63 +2,68 @@ package com.github.houseorganizer.houseorganizer.billsharer;
 
 import android.util.Log;
 
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Billsharer {
 
-    private ArrayList<Expense> expenses;
-    private ArrayList<Debt> debts;
-    private HashMap<String, Integer> balances;
+    private List<Expense> expenses;
+    private List<Debt> debts;
+    private Map<String, Integer> balances;
     private final DocumentReference currentHouse;
-    private ArrayList<String> residents;
+    private final DocumentReference onlineReference;
+    private List<String> residents;
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    public Billsharer(DocumentReference currentHouse) {
-        expenses = new ArrayList<>();
+    public Billsharer(DocumentReference currentHouse, DocumentReference onlineReference, List<Expense> expenses) {
+        this.expenses = expenses;
         debts = new ArrayList<>();
         this.currentHouse = currentHouse;
+        this.onlineReference = onlineReference;
         initResidents();
         initBalances();
         computeBalances();
     }
 
-    public ArrayList<Expense> getExpenses() {
+    public List<Expense> getExpenses() {
         return expenses;
     }
 
-    public void setExpenses(ArrayList<Expense> expenses) {
+    public void setExpenses(List<Expense> expenses) {
         this.expenses = expenses;
     }
 
-    public ArrayList<Debt> getDebts() {
+    public List<Debt> getDebts() {
         return debts;
     }
 
-    public void setDebts(ArrayList<Debt> debts) {
+    public void setDebts(List<Debt> debts) {
         this.debts = debts;
     }
 
-    public HashMap<String, Integer> getBalances() {
+    public Map<String, Integer> getBalances() {
         return balances;
     }
 
-    public void setBalances(HashMap<String, Integer> balances) {
+    public void setBalances(Map<String, Integer> balances) {
         this.balances = balances;
     }
 
-    public ArrayList<String> getResidents() {
+    public List<String> getResidents() {
         return residents;
     }
 
-    public void setResidents(ArrayList<String> residents) {
+    public void setResidents(List<String> residents) {
         this.residents = residents;
-    }
-
-    public void addExpense(Expense expense) {
-        expenses.add((Expense) expense.clone());
     }
 
     public void initResidents() {
@@ -105,5 +110,75 @@ public class Billsharer {
                 }
             }
         }
+    }
+
+    public void addExpense(Expense expense) {
+        expenses.add((Expense) expense.clone());
+        updateExpenses();
+    }
+
+    public void removeExpense(Expense expense) {
+        expenses.remove(expense);
+        updateExpenses();
+    }
+
+    public Task<Void> updateExpenses() {
+        if(currentHouse == null || onlineReference == null){
+            return Tasks.forCanceled();
+        }
+        List<Map<String, Object>> expenses = convertExpensesListToFirebase();
+
+        return onlineReference.update("expenses", expenses);
+    }
+
+    public Task<DocumentSnapshot> refreshExpenses(){
+        if(onlineReference == null){
+            return Tasks.forCanceled();
+        }
+        return onlineReference.get().continueWith( r -> {
+            if(!r.isSuccessful())return null;
+            DocumentSnapshot snap = r.getResult();
+            setExpenses(convertFirebaseListToExpenses((List<Map<String, Object>>) snap.get("expenses")));
+            return snap;
+        });
+    }
+
+    private List<Map<String, Object>> convertExpensesListToFirebase(){
+        List<Map<String, Object>> result = new ArrayList<>();
+        for(Expense expense : expenses){
+            Map<String, Object> expenseMap = new HashMap<>();
+            expenseMap.put("cost", expense.getCost());
+            expenseMap.put("payee", expense.getPayee());
+            expenseMap.put("shares", expense.getShares());
+            expenseMap.put("title", expense.getTitle());
+            result.add(expenseMap);
+        }
+        return result;
+    }
+
+    private static List<Expense> convertFirebaseListToExpenses(List<Map<String, Object>> list){
+        List<Expense> expenses = new ArrayList<>();
+        for(Map<String, Object> m : list){
+            expenses.add(new Expense((String)m.get("title"), (int)m.get("cost"), (String)m.get("payee"),
+                    (HashMap<String, Integer>)m.get("shares")));
+        }
+        return expenses;
+    }
+
+    public static Task<Billsharer> retrieveBillsharer(CollectionReference billsharerRoot, DocumentReference household){
+        return billsharerRoot.whereEqualTo("household", household).get().continueWith( t -> {
+            List<DocumentSnapshot> res = t.getResult().getDocuments();
+            if(res.isEmpty())return null;
+            if(res.size() > 1) throw new IllegalStateException("More than one billsharer for this house");
+            return buildBillsharer(res.get(0));
+        });
+    }
+
+    public static Billsharer buildBillsharer(DocumentSnapshot documentSnapshot){
+        if(documentSnapshot == null) return null;
+        DocumentReference household = (DocumentReference) documentSnapshot.get("household");
+        List<Map<String, Object>> list = (List<Map<String, Object>>) documentSnapshot.get("expenses");
+        List<Expense> expenses = convertFirebaseListToExpenses(list);
+        return new Billsharer(household, documentSnapshot.getReference(), expenses);
     }
 }
