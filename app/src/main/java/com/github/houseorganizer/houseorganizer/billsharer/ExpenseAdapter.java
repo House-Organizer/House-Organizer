2ec175c8b4c4b6492d532e3a170f950746b9737a
germano <maxime.germano@epfl.ch>
2022-04-28 18:55:01 +0200

    Replace ViewHolder with ExpenseViewHolder

--- app/src/main/java/com/github/houseorganizer/houseorganizer/billsharer/ExpenseAdapter.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/billsharer/ExpenseAdapter.java
@@ -1,16 +1,21 @@
 package com.github.houseorganizer.houseorganizer.billsharer;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.houseorganizer.houseorganizer.R;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class ExpenseAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder> {
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private Billsharer billsharer;
+    private final Billsharer billsharer;
 
     public ExpenseAdapter(Billsharer billsharer) {
         this.billsharer = billsharer;
@@ -18,11 +23,7 @@ public class ExpenseAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     public void addExpense() {}
 
-    public void pushExpense() {}
-
     public void editExpense() {}
-
-    public void createExpense() {}
 
     public void deleteExpense() {}
 
@@ -30,17 +31,32 @@ public class ExpenseAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ExpenseAdapter.ExpenseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        return new ExpenseViewHolder(inflater.inflate(R.layout.expense_cell, parent));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ExpenseAdapter.ExpenseViewHolder holder, int position) {
+        Expense expense = billsharer.getExpenses().get(position);
+        ((ExpenseViewHolder) holder).textView.setText(expense.toText());
     }
 
     @Override
     public int getItemCount() {
         return billsharer.getExpenses().size();
     }
+
+    public static class ExpenseViewHolder extends RecyclerView.ViewHolder {
+        public Button textView;
+        // TODO there will be a title, a cost, a payee, and the date
+
+        public ExpenseViewHolder(View eventView) {
+            super(eventView);
+
+            // TODO create layout of view
+        }
+    }
+
 }
