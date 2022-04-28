c127c9c62cb7106e4bc0807e3d7a9a16a766f504
Sandra <sansive00@gmail.com>
2022-04-28 12:18:54 +0200

    requestPermissions

--- app/src/main/java/com/github/houseorganizer/houseorganizer/house/HouseSelectionActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/house/HouseSelectionActivity.java
@@ -80,7 +80,7 @@ public class HouseSelectionActivity extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // Permissions not granted yet
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+                //requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
             }
         }
     }
