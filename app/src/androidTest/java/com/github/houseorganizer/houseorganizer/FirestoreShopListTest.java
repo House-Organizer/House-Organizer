39e330caae2f996c316c3a96f66fc690fa11d1ea
Luca Delabarre <luca.delabarre@epfl.ch>
2022-05-04 12:14:49 +0200

    Attempt fix groceries test

--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/FirestoreShopListTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/FirestoreShopListTest.java
@@ -9,6 +9,7 @@ import com.github.houseorganizer.houseorganizer.shop.FirestoreShopList;
 import com.github.houseorganizer.houseorganizer.shop.ShopItem;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -26,7 +27,7 @@ import java.util.concurrent.ExecutionException;
 @RunWith(AndroidJUnit4.class)
 public class FirestoreShopListTest {
 
-    private static FirestoreShopList localShopList;
+    private static FirestoreShopList shopList;
     private static FirebaseFirestore db;
 
     @BeforeClass
@@ -34,59 +35,60 @@ public class FirestoreShopListTest {
         FirebaseTestsHelper.startAuthEmulator();
         FirebaseTestsHelper.startFirestoreEmulator();
         FirebaseTestsHelper.setUpFirebase();
+
         db = FirebaseFirestore.getInstance();
+
         Task<QuerySnapshot> t = db.collection("shop_lists").get();
         Tasks.await(t);
         assertThat(t.getResult().getDocuments().size() > 0, is(true));
-        DocumentSnapshot snap = t.getResult().getDocuments().get(1);
-        List<Map<String, Object>> map = (List<Map<String, Object>>) snap.get("items");
-        assertThat(map.isEmpty(), is(false));
-        String name = (String) map.get(0).get("name");
-        int quantity = new Long((long) map.get(0).get("quantity")).intValue();
-        String unit = (String) map.get(0).get("unit");
-        List<ShopItem> list = new ArrayList<>();
-        list.add(new ShopItem(name, quantity, unit));
-        localShopList = new FirestoreShopList(db.collection("households")
-                .document(FirebaseTestsHelper.TEST_HOUSEHOLD_NAMES[0]),
-                snap.getReference(), list);
+
+        // Store new shop list with one item for TEST_HOUSEHOLD_NAMES[2] on Firebase
+        DocumentReference household = db.collection("households")
+                .document(FirebaseTestsHelper.TEST_HOUSEHOLD_NAMES[2]);
+        shopList = new FirestoreShopList(household);
+        shopList.addItem(FirebaseTestsHelper.TEST_ITEM);
+        Task<DocumentReference> t1 = FirestoreShopList.storeNewShopList(db.collection("shop_lists"), shopList, household);
+        Tasks.await(t1);
+        shopList.setOnlineReference(t1.getResult());
     }
 
     @Test
     public void storeNewShopListWorks() throws ExecutionException, InterruptedException {
-        Task<DocumentSnapshot> t = localShopList.getOnlineReference().get();
-        Tasks.await(t);
-        assertThat(t.getResult().get("household"), is(localShopList.getHousehold()));
+        // Get stored shop list from Firebase
+        Task<DocumentSnapshot> t2 = shopList.getOnlineReference().get();
+        Tasks.await(t2);
+
+        assertThat(t2.getResult().get("household"), is(shopList.getHousehold()));
     }
 
     @Test
     public void retrieveShopListWorksFor() throws ExecutionException, InterruptedException {
+        DocumentReference household = db.collection("households")
+                .document(FirebaseTestsHelper.TEST_HOUSEHOLD_NAMES[2]);
         Task<FirestoreShopList> t = FirestoreShopList.
-                retrieveShopList(db.collection("shop_lists"),
-                        localShopList.getHousehold());
+                retrieveShopList(db.collection("shop_lists"), household);
         Tasks.await(t);
         FirestoreShopList nList = t.getResult();
-        assertThat(nList.getOnlineReference(),
-                is(localShopList.getOnlineReference()));
-        assertThat(nList.getHousehold(), is(localShopList.getHousehold()));
-        assertThat(nList.isEmpty(), is(localShopList.isEmpty()));
-        assertThat(localShopList.size(), is(localShopList.size()));
+        assertThat(nList.getOnlineReference(), is(shopList.getOnlineReference()));
+        assertThat(nList.getHousehold(), is(shopList.getHousehold()));
+        assertThat(nList.isEmpty(), is(shopList.isEmpty()));
+        assertThat(shopList.size(), is(shopList.size()));
+
         // Testing retrieved item
-        ShopItem rItem = localShopList.getItemAt(0);
+        ShopItem rItem = shopList.getItemAt(0);
         assertThat(rItem.getName(), is(FirebaseTestsHelper.TEST_ITEM.getName()));
         assertThat(rItem.getUnit(), is(FirebaseTestsHelper.TEST_ITEM.getUnit()));
         assertThat(rItem.getQuantity(), is(FirebaseTestsHelper.TEST_ITEM.getQuantity()));
     }
 
-
-
     @Test
     public void updateItemsWorks() throws ExecutionException, InterruptedException {
         ShopItem testItem = new ShopItem("Raclette", 2, "g");
-        localShopList.addItem(testItem);
-        localShopList.updateItems();
+        shopList.addItem(testItem);
+        shopList.updateItems();
         Task<FirestoreShopList> t = FirestoreShopList.
                 retrieveShopList(db.collection("shop_lists"),
-                        localShopList.getHousehold());
+                        shopList.getHousehold());
         Tasks.await(t);
         FirestoreShopList nList = t.getResult();
         assertThat(nList.size(), is(2));
@@ -98,26 +100,25 @@ public class FirestoreShopListTest {
     @Test
     public void refreshItemWorks() throws ExecutionException, InterruptedException {
         Task<FirestoreShopList> t = FirestoreShopList.retrieveShopList(
-                db.collection("shop_lists"), localShopList.getHousehold());
+                db.collection("shop_lists"), shopList.getHousehold());
         Tasks.await(t);
         FirestoreShopList listBis = t.getResult();
 
         ShopItem item = new ShopItem("Raclette", 2, "g");
         listBis.addItem(item);
-        Task<DocumentSnapshot> t1 = listBis.updateItems().continueWithTask(r -> localShopList.refreshItems());
+        Task<DocumentSnapshot> t1 = listBis.updateItems().continueWithTask(r -> shopList.refreshItems());
         Tasks.await(t1);
-        assertThat(localShopList.size(), is(listBis.size()));
-        assertThat(localShopList.getItemAt(0).getName(), is(listBis.getItemAt(0).getName()));
+        assertThat(shopList.size(), is(listBis.size()));
+        assertThat(shopList.getItemAt(0).getName(), is(listBis.getItemAt(0).getName()));
     }
 
     @After
     public void removeItems(){
-        if(localShopList.size() > 1){
-            for(int i = localShopList.size()-1; i > 0; --i){
-                localShopList.removeItem(i);
+        if(shopList.size() > 1){
+            for(int i = shopList.size()-1; i > 0; --i){
+                shopList.removeItem(i);
             }
-            localShopList.updateItems();
+            shopList.updateItems();
         }
     }
-
 }
