28a698e693db01bd77ee756f5438ebc126f0e347
Geno22 <florian.marcoz@outlook.fr>
2022-05-11 14:25:35 +0200

    Started testing

--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/CreateHouseholdActivityTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/CreateHouseholdActivityTest.java
@@ -1,18 +1,26 @@
 package com.github.houseorganizer.houseorganizer;
 
+import static android.app.PendingIntent.getActivity;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.github.houseorganizer.houseorganizer.FirebaseTestsHelper.TEST_HOUSEHOLD_NAMES;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertTrue;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
 
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -38,6 +46,7 @@ import java.util.concurrent.ExecutionException;
 public class CreateHouseholdActivityTest {
     private static FirebaseFirestore db;
     private static FirebaseAuth auth;
+    private View decorView;
 
     @BeforeClass
     public static void createMockFirebase() throws ExecutionException, InterruptedException {
@@ -69,19 +78,38 @@ public class CreateHouseholdActivityTest {
 
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         context.sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
+        createHouseholdRule.getScenario().onActivity(a -> decorView = a.getWindow().getDecorView());
     }
 
     @Test
     public void createHouseholdWorks() throws ExecutionException, InterruptedException {
+
         onView(withId(R.id.editTextHouseholdName)).perform(click(),
-                typeText("Test"), closeSoftKeyboard());
+                typeText("MyHouse"), closeSoftKeyboard());
+        onView(withId(R.id.editTextAddress)).perform(
+                typeText("EPFL, Lausanne"), closeSoftKeyboard());
+        onView(withId(R.id.submitHouseholdButton)).perform(click());
+        Thread.sleep(2000);
+        onView(withText(R.string.confirm)).perform(click());
+
+        assertTrue(FirebaseTestsHelper.householdExists("MyHouse", db));
+
+        /*
         onView(withId(R.id.editTextLatitude)).perform(click(),
                 typeText("45"), closeSoftKeyboard());
         onView(withId(R.id.editTextLongitude)).perform(click(),
                 typeText("45"), closeSoftKeyboard());
-        onView(withId(R.id.submitHouseholdButton)).perform(click());
 
-        assertTrue(FirebaseTestsHelper.householdExists(TEST_HOUSEHOLD_NAMES[0], db));
+
+        */
+    }
+
+    @Test
+    public void emptyFieldsDoesNotCreateHouse(){
+        onView(withId(R.id.submitHouseholdButton)).perform(click());
+        onView(withText(R.string.address_fill_fields))
+                .inRoot(withDecorView(not(decorView)))
+                .check(matches(isDisplayed()));
     }
 
     @Test
