629d005ec76dc1c0c978ac79c272930203756d45
Luca Delabarre <luca.delabarre@epfl.ch>
2022-05-14 22:11:12 +0200

    Added languages translations for EN/FR/PT/SP. Italian will be done later. Not tested. Fixed themes not changing also. TODO refresh settings once language or theme is selected

--- app/src/main/java/com/github/houseorganizer/houseorganizer/panels/settings/ThemedAppCompatActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/panels/settings/ThemedAppCompatActivity.java
@@ -1,18 +1,34 @@
 package com.github.houseorganizer.houseorganizer.panels.settings;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import com.github.houseorganizer.houseorganizer.R;
-import com.github.houseorganizer.houseorganizer.util.Util;
+
+import java.util.Locale;
 
 public class ThemedAppCompatActivity extends AppCompatActivity {
+    private SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
         setTheme(getCurrentTheme());
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        Locale locale = new Locale(getCurrentLanguage());
+        Locale.setDefault(locale);
+        config.setLocale(locale);
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+    }
+
+    private String getCurrentLanguage() {
+        return sp.getString("lang", "en");
     }
 
     @Override
@@ -22,8 +38,7 @@ public class ThemedAppCompatActivity extends AppCompatActivity {
     }
 
     private @StyleRes int getCurrentTheme() {
-        String themeCode = Util.getSharedPrefs(this)
-                .getString("theme", "1");
+        String themeCode = sp.getString("theme", "1");
 
         switch (themeCode) {
             case "1": return R.style.Theme1_HouseOrganizer;
