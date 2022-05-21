496c419b94508cb7ccdddb765d63745f8db24dd5
Sandra <sansive00@gmail.com>
2022-05-21 11:48:56 +0200

    Remove idling resources from calendar

--- app/src/main/java/com/github/houseorganizer/houseorganizer/panels/main_activities/CalendarActivity.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/panels/main_activities/CalendarActivity.java
@@ -3,7 +3,6 @@ package com.github.houseorganizer.houseorganizer.panels.main_activities;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,28 +15,18 @@ import com.github.houseorganizer.houseorganizer.calendar.Calendar;
 import com.github.houseorganizer.houseorganizer.calendar.CalendarAdapter;
 import com.github.houseorganizer.houseorganizer.calendar.MonthlyAdapter;
 import com.github.houseorganizer.houseorganizer.calendar.UpcomingAdapter;
-import com.github.houseorganizer.houseorganizer.util.interfaces.RecyclerViewIdlingCallback;
-import com.github.houseorganizer.houseorganizer.util.interfaces.RecyclerViewLayoutCompleteListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.OptionalInt;
 
-public final class CalendarActivity extends NavBarActivity implements
-        ViewTreeObserver.OnGlobalLayoutListener,
-        RecyclerViewIdlingCallback {
+public final class CalendarActivity extends NavBarActivity {
 
     private CalendarAdapter calendarAdapter;
 
     private RecyclerView calendarEvents;
     private final Calendar calendar = new Calendar();
-
-    // Flag to indicate if the layout for the recyclerview has complete.
-    private boolean recyclerViewLayoutCompleted;
-    // Listener to be set by the idling resource, so that it can be notified when recyclerview
-    // layout has been done
-    private RecyclerViewLayoutCompleteListener listener;
 
     @SuppressLint("NotifyDataSetChanged")
     @Override
@@ -57,8 +46,6 @@ public final class CalendarActivity extends NavBarActivity implements
         calendarEvents.setAdapter(calendarAdapter);
         calendarEvents.setLayoutManager(new GridLayoutManager(this, 1));
         calendarAdapter.refreshCalendarView(this, currentHouse, "refreshCalendar:failureToRefresh", false);
-        recyclerViewLayoutCompleted = true;
-        calendarEvents.getViewTreeObserver().addOnGlobalLayoutListener(this);
 
         setUpViewChange(yearMonth, navigateMonthLeft, navigateMonthRight);
 
@@ -68,7 +55,6 @@ public final class CalendarActivity extends NavBarActivity implements
 
     private void setUpViewChange(TextView yearMonth, Button navigateMonthLeft, Button navigateMonthRight) {
         findViewById(R.id.calendar_screen_view_change).setOnClickListener(v -> {
-            recyclerViewLayoutCompleted = false;
             calendarAdapter = calendar.rotateCalendarView(this, calendarAdapter, calendarEvents);
             calendarAdapter.refreshCalendarView(this, currentHouse, "refreshCalendar:failureToRefresh", calendar.getView() == Calendar.CalendarView.MONTHLY);
             yearMonth.setText(YearMonth.now().format(DateTimeFormatter.ofPattern("LLLL uuuu")));
@@ -97,33 +83,5 @@ public final class CalendarActivity extends NavBarActivity implements
     @Override
     protected CurrentActivity currentActivity() {
         return CurrentActivity.CALENDAR;
-    }
-
-    @Override
-    public void onGlobalLayout() {
-        if (listener != null) {
-            // Set flag to let the idling resource know that processing has completed and is now idle
-            recyclerViewLayoutCompleted = true;
-
-            // Notify the listener (should be in the idling resource)
-            listener.onLayoutCompleted();
-        }
-    }
-
-    @Override
-    public void setRecyclerViewLayoutCompleteListener(RecyclerViewLayoutCompleteListener listener) {
-        this.listener = listener;
-    }
-
-    @Override
-    public void removeRecyclerViewLayoutCompleteListener(RecyclerViewLayoutCompleteListener listener) {
-        if (this.listener != null && this.listener == listener) {
-            this.listener = null;
-        }
-    }
-
-    @Override
-    public boolean isRecyclerViewLayoutCompleted() {
-        return recyclerViewLayoutCompleted;
     }
 }
