fe960abfb564e37dad3652d92d1ee0a909e3b2db
Geno22 <florian.marcoz@outlook.fr>
2022-05-21 16:10:49 +0200

    Trying to fix groceries tests v2

--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/GroceriesActivityTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/GroceriesActivityTest.java
@@ -73,7 +73,7 @@ public class GroceriesActivityTest {
 
     private void addNewItem(String name, int quantity, String unit) throws InterruptedException {
         onView(withId(R.id.groceries_add)).perform(click());
-        Thread.sleep(500);
+        Thread.sleep(1000);
         onView(withId(R.id.edit_text_name)).perform(typeText(name));
         onView(withId(R.id.edit_text_quantity)).perform(typeText(""+quantity));
         onView(withId(R.id.edit_text_unit)).perform(typeText(unit));
