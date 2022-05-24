f750e17ccd5d89f90ab39e132bfa87f3a9dfc3d7
germano <maxime.germano@epfl.ch>
2022-05-24 15:45:28 +0200

    add text changed listener

--- app/src/main/java/com/github/houseorganizer/houseorganizer/billsharer/SharesAdapter.java
+++ app/src/main/java/com/github/houseorganizer/houseorganizer/billsharer/SharesAdapter.java
@@ -1,5 +1,8 @@
 package com.github.houseorganizer.houseorganizer.billsharer;
 
+import android.annotation.SuppressLint;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +17,20 @@ import com.github.houseorganizer.houseorganizer.R;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class SharesAdapter extends RecyclerView.Adapter<SharesAdapter.ShareHolder> {
 
     private final HashMap<String, Double> shares;
+    private final HashMap<String, Boolean> modified;
     private final List<String> user_emails;
 
     public SharesAdapter(HashMap<String, Double> shares) {
         this.shares = shares;
         user_emails = new ArrayList<>();
+        modified = new HashMap<>();
         shares.forEach((k, v) -> user_emails.add(k));
+        shares.forEach((k, v) -> modified.put(k, false));
     }
 
     @NonNull
@@ -34,19 +41,31 @@ public class SharesAdapter extends RecyclerView.Adapter<SharesAdapter.ShareHolde
         return new ShareHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ShareHolder holder, int position) {
         String user = user_emails.get(position);
         holder.user.setText(user);
-        holder.amount.setText(setShareAmount(holder));
+        holder.amount.setText(Objects.requireNonNull(shares.get(user)).toString());
+        holder.amount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                modified.put(user, true);
+                updateShares();
+            }
+        });
     }
 
-    private String setShareAmount(ShareHolder holder) {
-        if (holder.modified) {
-            return holder.amount.getText().toString();
-        } else {
-            return "";
-        }
+    private void updateShares() {
+
     }
 
     @Override
@@ -57,7 +76,6 @@ public class SharesAdapter extends RecyclerView.Adapter<SharesAdapter.ShareHolde
     public static class ShareHolder extends RecyclerView.ViewHolder {
         public TextView user;
         public EditText amount;
-        public boolean modified;
 
         public ShareHolder(@NonNull View itemView) {
             super(itemView);
