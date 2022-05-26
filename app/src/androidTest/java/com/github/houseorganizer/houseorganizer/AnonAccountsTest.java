89dbfe8dcdefa243764471ea65c2570dc1791b27
aindreias <anamaria.indreias@gmail.com>
2022-05-26 02:26:32 +0200

    Try to fix tests | Thread.sleep to wait for email update

--- app/src/androidTest/java/com/github/houseorganizer/houseorganizer/AnonAccountsTest.java
+++ app/src/androidTest/java/com/github/houseorganizer/houseorganizer/AnonAccountsTest.java
@@ -13,9 +13,13 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.github.houseorganizer.houseorganizer.panels.login.LoginActivity;
 import com.github.houseorganizer.houseorganizer.panels.main_activities.MainScreenActivity;
@@ -23,6 +27,7 @@ import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -70,6 +75,7 @@ public class AnonAccountsTest {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         assertNotNull(user);
 
+        Thread.sleep(300);
         assertEquals(user.getUid().hashCode() + "@house-org.com", user.getEmail());
 
         Intents.release();
